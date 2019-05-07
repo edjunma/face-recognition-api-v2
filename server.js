c33c75afe6c48dffd769337633dc067389476515
edjunma <junwenma@buffalo.edu>
2019-05-07 03:49:52 -0400

    5/7 update, connect back to front

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get("/profile/:id", (req, res) => {
 	}
 });
 
-app.post("/image", (req, res) => {
+app.put("/image", (req, res) => {
 	const { id } = req.params;
 	let found = false;
 	database.users.forEach(user => {
