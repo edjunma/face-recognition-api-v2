aac56c92dadda9ca63f893d9e7c5cabd5f8fda6a
edjunma <junwenma@buffalo.edu>
2019-08-18 04:02:50 -0400

    added database connection with process.env

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: 'postgresql-perpendicular-75228',
-		user: 'edjunma',
-		password: '',
-		database: 'smart-brain'
+		host: process.env.DATABASE_URL,
+		ssl: true
 	}
 });
 
