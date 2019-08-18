2bf25462ce8d1954b2bfe63475d87764eaa89b6c
edjunma <junwenma@buffalo.edu>
2019-08-18 05:05:50 -0400

    debugged

--- controllers/image.js
+++ controllers/image.js
@@ -18,7 +18,7 @@ const handleImg = (req, res, db) => {
 	const { id } = req.body;
 	db('users')
 		.where('id', '=', id)
-		.increment('entries', 1)v
+		.increment('entries', 1)
 		.returning('entries')
 		.then(entries => {
 			res.json(entries[0]);
--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const db = knex({
 });
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(cors());
 
