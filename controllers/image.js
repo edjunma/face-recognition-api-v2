ded74f54d8367b5cc233a557d9cd8dd79087c496
edjunma <junwenma@buffalo.edu>
2019-08-24 23:59:44 -0400

    latest update

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 // You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
-	apiKey: 'YOUR_API_KEY_HERE'
+	apiKey: '147046a299a541b4902c4001df6535a2'
 });
 
 const handleApiCall = (req, res) => {
